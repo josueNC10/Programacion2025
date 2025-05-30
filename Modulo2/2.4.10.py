bbb90136991761600969681574be663bffc0bd9a
josueNC10 <jnavarroc@ctpplatanares.ed.cr>
2025-05-30 16:16:15 -0600

    python 30/05/2025 notebook 02

---
+++ Modulo2/2.4.10.py
@@ -0,0 +1,15 @@
+#Nombre: josue navarro camacho
+x =  0
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
+
+x = 1
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print('y =', y)
+
+x = -1
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print('y =', y)
---
+++ Modulo2/2.6.9.py
@@ -0,0 +1,18 @@
+#Nombre: Josue Navarro Camacho
+a = float(input("Ingresa el primer valor: "))
+b = float(input("Ingresa el segundo valor: "))
+
+# mostrar el resultado de la suma aquí
+suma = a + b
+print('El resultado de la suma es:',suma)
+# mostrar el resultado de la resta aquí
+resta = a - b 
+print('El resultado de la resta es:',resta)
+# mostrar el resultado de la multiplicación aquí
+multiplicion = a * b
+print('El resultado de la multiplicacion es:',multiplicion)
+# mostrar el resultado de la división aquí
+division = a / b
+print('El resultado de la division es:',division)
+
+print("\n¡Eso es todo, amigos!")
